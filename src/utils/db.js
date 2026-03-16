5a3ef31ecb9b3884395fbe7144acea2fcb78b4cd
nicolas <nicolasbernard19@gmail.com>
2026-03-16 23:21:30 +0000

    rename db.js to connect.js in utils

--- src/utils/db.js
+++
@@ -1,8 +0,0 @@
-import pg from "pg"
-
-export function connect() {
-    return new pg.Pool({connectionString: process.env.DB_CONN})
-}
-// to use it 
-// import {connect} from "@/utils"
-const db = connect()
